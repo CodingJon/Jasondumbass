6b042405382f6704ac7057f09606b2ea2705d1ef
CodingJon <75001826+CodingJon@users.noreply.github.com>
2022-02-09 15:55:43 -0500

    Add files via upload

---
+++ jason/client.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+	while true do
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_UNARMED"), 0.2)
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_PISTOL_MK2"), 0.8)
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_SNSPISTOL"), 0.55)
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_BULLPUPRIFLE"), 0.6)
+	Wait(0)
+    end
+end)
+
+
+--https://vespura.com/fivem/weapons/-- --MODEL NAME ONLY--
